eb297f8623b2b27eb1feec241db4cee01e0822e9
cvanem <chris@greenlinkservices.com>
2020-09-04 22:36:15 +0000

    Deploying to gh-pages from  @ 4f25e7e39610b9dd4231e6009e9869d1775c4120 🚀

---
+++ precache-manifest.55a3d88613d084bc4b5270d5056be811.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a77610d92177e67334d7c670e1f38e72",
+    "url": "/doors/index.html"
+  },
+  {
+    "revision": "63881e64ccf159f16eb6",
+    "url": "/doors/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "996605fd863065a9396c",
+    "url": "/doors/static/js/2.d110fc29.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/doors/static/js/2.d110fc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63881e64ccf159f16eb6",
+    "url": "/doors/static/js/main.97be0598.chunk.js"
+  },
+  {
+    "revision": "8a361f9bf7b53126220a",
+    "url": "/doors/static/js/runtime-main.ae110295.js"
+  },
+  {
+    "revision": "83e76d20694d1c69568e48494bcdadfd",
+    "url": "/doors/static/media/logo.83e76d20.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3aeda8b3f875646541630c9b841f938f.js"
+  "/doors/precache-manifest.55a3d88613d084bc4b5270d5056be811.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doors/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
