fb6e1f1a8eb0ba5d3799d70a7bf3db1fe40173bb
Chris Van Emmerik <chris@greenlinkservices.com>
2020-09-07 19:06:35 -0600

    [InstructorTable] Add test data

--- src/components/application/GenericTable/Instructors/hooks.tsx
+++ src/components/application/GenericTable/Instructors/hooks.tsx
@@ -22,9 +22,9 @@ export const useDeleteInstructors = ({ instructors, onSuccess: OnSuccess = undef
 
 export const useInstructors = (table, tab) => {
   const instructors = [
-    { id: '1', Name: 'Test Instructor 1', Institution: 'Test Institution 1' },
-    { id: '2', Name: 'Test Instructor 2', Institution: 'Test Institution 2' },
-    { id: '3', Name: 'Test Instructor 3', Institution: 'Test Institution 3' }
+    { id: '1', Name: 'Test Instructor 1', Title: 'Test Title 1', Institution: 'Test Institution 1' },
+    { id: '2', Name: 'Test Instructor 2', Title: 'Test Title 2', Institution: 'Test Institution 2' },
+    { id: '3', Name: 'Test Instructor 3', Title: 'Test Title 3', Institution: 'Test Institution 3' }
   ];
 
   return useTableFilter(
