d2bdc996c7b9bfbd6a64cccd9c01fe33bc47eb4c
Chris Van Emmerik <chris@greenlinkservices.com>
2020-09-18 10:27:50 -0600

    Implement useS3Bucket and connect to backend

---
+++ src/database/useS3Bucket.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import awsconfig from '../aws-exports';
+import Amplify, { Storage } from 'aws-amplify';
+import { uuid } from '../helpers';
+Amplify.configure(awsconfig);
+
+export default function useS3Bucket({ setState = undefined }) {
+  const handleUpload = React.useCallback(
+    ({
+      id = `unknown_${uuid()}`,
+      content = undefined,
+      level = 'public', // public or private
+      contentType = 'text/plain',
+      onStart = undefined,
+      onSuccess = undefined,
+      onError = undefined
+    }) => {
+      console.log('Uploading file...');
+      onStart && onStart();
+      setState && setState(prev => ({ ...prev, loading: true, success: false }));
+      Storage.put(id, content, {
+        level,
+        contentType
+      })
+        .then(result => {
+          console.log({ status: 'Successfully uploaded file', result });
+          onSuccess && onSuccess(result);
+          setState && setState(prev => ({ ...prev, loading: false, success: true }));
+        })
+        .catch(err => {
+          console.error({ status: 'Error uploading file', err });
+          onError && onError(err);
+          setState && setState(prev => ({ ...prev, loading: false, success: false }));
+        });
+    },
+    []
+  );
+
+  return { handleUpload };
+}
