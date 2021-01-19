14c2609c496c8e1a5037e73d705adc235a06f7aa
cvanem <chris@greenlinkservices.com>
2021-01-19 19:34:38 +0000

    Deploying to gh-pages from  @ d8ffeb26f1e428ac01fdc386db9ca597517994b9 🚀

---
+++ precache-manifest.f13a5f0be098f6909ebca5eb16c3d51b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2993997270f18864b3abb1bb8005e96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20e7cc526676f574b0a8",
+    "url": "/static/css/2.c917d85d.chunk.css"
+  },
+  {
+    "revision": "77a90c7c6a0444f181ba",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "20e7cc526676f574b0a8",
+    "url": "/static/js/2.c2e1aef3.chunk.js"
+  },
+  {
+    "revision": "63936b11e7aa6d32c74fe1c9af765ebd",
+    "url": "/static/js/2.c2e1aef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af139fd032cd92a12173",
+    "url": "/static/js/3.b38dadb7.chunk.js"
+  },
+  {
+    "revision": "8da2a0568153c3fccc17",
+    "url": "/static/js/4.d004fabf.chunk.js"
+  },
+  {
+    "revision": "77a90c7c6a0444f181ba",
+    "url": "/static/js/main.80b46c9d.chunk.js"
+  },
+  {
+    "revision": "dd5f5c35ad142e2fd054",
+    "url": "/static/js/runtime-main.c488f3ae.js"
+  },
+  {
+    "revision": "6e5293f16322844295b39f9d0d0c4eb5",
+    "url": "/static/media/Montserrat-Regular.6e5293f1.woff2"
+  },
+  {
+    "revision": "1311922fe001df6e1e8465643d56611b",
+    "url": "/static/media/archivo-v7-latin-regular.1311922f.woff2"
+  },
+  {
+    "revision": "3f0d16c3d5224c4e6ec5ee2f091829fd",
+    "url": "/static/media/group.3f0d16c3.png"
+  },
+  {
+    "revision": "f10b510b9ba5a25dc72b59320400e9f3",
+    "url": "/static/media/logo.f10b510b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.547060bcff3113212624643426542f59.js"
+  "/precache-manifest.f13a5f0be098f6909ebca5eb16c3d51b.js"
 );
 
 self.addEventListener('message', (event) => {
